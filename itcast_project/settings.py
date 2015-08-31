228ac99ca2ad2574f092331fb5b64437cf727ac3
xingwenpeng <xingwenpeng@itcast.cn>
2015-08-31 17:16:07 +0800

    model

--- itcast_project/settings.py
+++ itcast_project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIRS = [
+    TEMPLATE_PATH,
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -82,3 +87,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+        STATIC_PATH,
+    )
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
+#def index(request):
+	#return HttpResponse("<br />itcast cpp hello world by xingwenpeng!<hr />")
 def index(request):
-	return HttpResponse("<br />itcast cpp hello world by xingwenpeng!<hr />")
+	category_list = Category.objects.all()
+	context_dict = {'categories':category_list}
+	return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+	pengchong_dict = {'pengchong': "How old ary you!"}
+	return render(request, 'rango/about.html', pengchong_dict)
